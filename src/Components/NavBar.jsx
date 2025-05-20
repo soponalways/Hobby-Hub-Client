533a99cca3a71ff9e5c99389489350be2bf05e6a
soponalways <soponalways@gmail.com>
2025-05-20 14:14:16 +0600

    Navbar done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router';
+import Logo from '/logo.png'
 
 const NavBar = () => {
+    const listItem = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/">All Groups</NavLink></li>
+        <li><NavLink to="/">Create Group</NavLink></li>
+        <li><NavLink to="/">My Groups</NavLink></li>
+    </>
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="navbar-start">
@@ -11,36 +19,23 @@ const NavBar = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {listItem}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <div className='w-6 md:w-8 lg:w-12'>
+                    <Link to='/'><img src={Logo} alt="Hobby Hub" /></Link>
+                </div>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {listItem}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <div className='flex gap-4 md:gap-6 lg:gap-8'>
+                    <NavLink to="/">Login</NavLink>
+                    <NavLink to="/">Register</NavLink>
+                </div>
             </div>
         </div>
     );
