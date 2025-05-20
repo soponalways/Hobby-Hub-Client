3e5cb68dac0aefd59080c30c063d8c5dfbe8fed6
soponalways <soponalways@gmail.com>
2025-05-21 00:30:05 +0600

    login page functionality done

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
 import { auth } from '../Firebase/firebase.config';
 
 const AuthProvider = ({ children }) => {
@@ -9,10 +9,14 @@ const AuthProvider = ({ children }) => {
     }; 
     const popUpSignIn = provider => {
         return signInWithPopup(auth, provider); 
+    }; 
+    const passwordSignIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password); 
     }
     const userInfo = {
         createUser, 
-        popUpSignIn
+        popUpSignIn, 
+        passwordSignIn
     }
     return (
         <AuthContext value={userInfo}>
